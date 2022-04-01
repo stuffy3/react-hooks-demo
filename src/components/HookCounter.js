2dd589078bdfa595e6d78d7cf5ca18c78520d69f
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2022-04-01 13:45:58 -0600

    initialize repo

---
+++ src/components/HookCounter.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export default function HookCounter() {
+    const [counter, setCounter] = useState(0)
+
+  return (
+    <div>
+        <button onClick={() => setCounter(counter + 1)}>Count {counter}</button>
+    </div>
+  )
+}
